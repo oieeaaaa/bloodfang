74f256c00aafa735960680021dcf54caafcf7d0d
Joimee Tan Cajandab <joimee.cajandab@codeantheory.com>
2021-01-06 21:29:00 +0800

    ✨ Init commit

---
+++ main.js
@@ -0,0 +1,206 @@
+window.addEventListener("DOMContentLoaded", () => {
+  // polyfills
+  let speechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+  let speechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+
+  // Payloads
+  const colors = [
+    "aliceblue",
+    "antiquewhite",
+    "aqua",
+    "aquamarine",
+    "azure",
+    "beige",
+    "bisque",
+    "black",
+    "blanchedalmond",
+    "blue",
+    "blueviolet",
+    "brown",
+    "burlywood",
+    "cadetblue",
+    "chartreuse",
+    "chocolate",
+    "coral",
+    "cornflowerblue",
+    "cornsilk",
+    "crimson",
+    "cyan",
+    "darkblue",
+    "darkcyan",
+    "darkgoldenrod",
+    "darkgray",
+    "darkgreen",
+    "darkgrey",
+    "darkkhaki",
+    "darkmagenta",
+    "darkolivegreen",
+    "darkorange",
+    "darkorchid",
+    "darkred",
+    "darksalmon",
+    "darkseagreen",
+    "darkslateblue",
+    "darkslategray",
+    "darkslategrey",
+    "darkturquoise",
+    "darkviolet",
+    "deeppink",
+    "deepskyblue",
+    "dimgray",
+    "dimgrey",
+    "dodgerblue",
+    "firebrick",
+    "floralwhite",
+    "forestgreen",
+    "fuchsia",
+    "gainsboro",
+    "ghostwhite",
+    "gold",
+    "goldenrod",
+    "gray",
+    "green",
+    "greenyellow",
+    "grey",
+    "honeydew",
+    "hotpink",
+    "indianred",
+    "indigo",
+    "ivory",
+    "khaki",
+    "lavender",
+    "lavenderblush",
+    "lawngreen",
+    "lemonchiffon",
+    "lightblue",
+    "lightcoral",
+    "lightcyan",
+    "lightgoldenrodyellow",
+    "lightgray",
+    "lightgreen",
+    "lightgrey",
+    "lightpink",
+    "lightsalmon",
+    "lightseagreen",
+    "lightskyblue",
+    "lightslategray",
+    "lightslategrey",
+    "lightsteelblue",
+    "lightyellow",
+    "lime",
+    "limegreen",
+    "linen",
+    "magenta",
+    "maroon",
+    "mediumaquamarine",
+    "mediumblue",
+    "mediumorchid",
+    "mediumpurple",
+    "mediumseagreen",
+    "mediumslateblue",
+    "mediumspringgreen",
+    "mediumturquoise",
+    "mediumvioletred",
+    "midnightblue",
+    "mintcream",
+    "mistyrose",
+    "moccasin",
+    "navajowhite",
+    "navy",
+    "oldlace",
+    "olive",
+    "olivedrab",
+    "orange",
+    "orangered",
+    "orchid",
+    "palegoldenrod",
+    "palegreen",
+    "paleturquoise",
+    "palevioletred",
+    "papayawhip",
+    "peachpuff",
+    "peru",
+    "pink",
+    "plum",
+    "powderblue",
+    "purple",
+    "rebeccapurple",
+    "red",
+    "rosybrown",
+    "royalblue",
+    "saddlebrown",
+    "salmon",
+    "sandybrown",
+    "seagreen",
+    "seashell",
+    "sienna",
+    "silver",
+    "skyblue",
+    "slateblue",
+    "slategray",
+    "slategrey",
+    "snow",
+    "springgreen",
+    "steelblue",
+    "tan",
+    "teal",
+    "thistle",
+    "tomato",
+    "turquoise",
+    "violet",
+    "wheat",
+    "white",
+    "whitesmoke",
+    "yellow",
+    "yellowgreen",
+  ];
+
+  const grammar =
+    "#JSGF V1.0; grammar colors; public <color> = " + colors.join(" | ") + " ;";
+
+  // init
+  const recognition = new speechRecognition();
+  const speechRecognitionList = new speechGrammarList();
+
+  //  SpeechRecognitionList configs
+  speechRecognitionList.addFromString(grammar, 1);
+
+  // Recognition configs
+  recognition.grammars = speechRecognitionList;
+  recognition.continuous = false;
+  recognition.lang = "en-US";
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  // Selectors
+  const results = document.getElementById("results");
+  const startButton = document.querySelector(".button--start");
+
+  // Listeners
+  startButton.addEventListener("click", () => {
+    startButton.innerText = "Listening...";
+
+    // start litening
+    recognition.start();
+  });
+
+  recognition.addEventListener("result", (e) => {
+    const { transcript } = e.results[0][0];
+    results.innerHTML = transcript;
+
+    const selectedColor = transcript.replace(/\s+/g, "").toLowerCase();
+
+    console.log(selectedColor);
+
+    const color = colors.find((c) => c === selectedColor);
+
+    if (color) {
+      results.style.backgroundColor = color;
+    }
+  });
+
+  recognition.addEventListener("speechend", () => {
+    startButton.innerText = "Start";
+    recognition.stop();
+  });
+});
